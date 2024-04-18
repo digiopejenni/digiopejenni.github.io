a66a658ec46d3727428104b2319dc691f08f55e0
Jenni Vanhanen <jenni.vanhanen@luksia.fi>
2024-04-18 13:20:17 +0300

    PÄivitykset laatikkoon

--- js-rocket/js/script.js
+++ js-rocket/js/script.js
@@ -1,5 +1,19 @@
+//tehdään muuttuja doCoolStuff, jota käytetään html:ssä funktiona
 var doCoolStuff = function (){
+//määritetään muuttuja tämänhetkinen luokan nimi ja haetaan sille html-elementistä class-name arvo
+    var currentClassName = document.getElementById('cool').className;
+// jos äsken hakemamme arvo on 'cool', vaihdetaan se 'cool red' .arvoon
+
+    if (currentClassName == 'cool'){
+        document.getElementById('cool').className= 'cool red';
+    }
+//jos äsken hakemamme arvo ei ole 'cool', päivitetään se arvoon 'cool'
+    else{
+        document.getElementById('cool').className = 'cool';
+    }
+    /*changing class name
     document.getElementById('cool').className = 'cool red';
+    */
 }
 
 var car = {
