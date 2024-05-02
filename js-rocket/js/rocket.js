a2f1435fdcec144b1f50f51a4935fe2c73137986
Jenni Vanhanen <jenni.vanhanen@luksia.fi>
2024-05-02 12:59:30 +0300

    Raketti lentää!

--- js-rocket/js/rocket.js
+++ js-rocket/js/rocket.js
@@ -1,30 +1,46 @@
 var timer = null;
 var countdownNumber = 10;
 
+
 var changeState= function(state){
     document.body.className = 'body-state'+ state;
     clearInterval(timer);
+    countdownNumber = 10;
+    //console.log(countdownNumber)
+    document.getElementById('countdown').innerHTML= countdownNumber;
+
     if(state == 2) {
         timer = setInterval(function() {
-            document.getElementById('countdown').innerHTML= countdownNumber;
             countdownNumber = countdownNumber -1;
+            document.getElementById('countdown').innerHTML= countdownNumber;
+            if (countdownNumber >4 && countdownNumber <= 7){
+                //be nervous
+                document.getElementById('nervous').className = 'nervous show';
+            } else{
+                document.getElementById('nervous').className = 'nervous';
+            }
+            if (countdownNumber > 1 && countdownNumber<= 4){
+                //can't wait
+                document.getElementById('cant-wait').className = 'cant-wait show';
+            } else{
+                document.getElementById('cant-wait').className = 'cant-wait';
+            }
+
             if (countdownNumber <=0){
                 changeState(3);
             }
-        }, 200);
-        
+        }, 200);  
     }
     else if (state == 3) {
         var success = setTimeout (function(){
             var randomNumber = Math.round(Math.random()*10);
-
-            if (randomNumber > 5) {
-                //do something
+            console.log('Lottoarvonta:', randomNumber)
+            // success
+            if (randomNumber > 2) {
+                changeState(4);
             } else{
-                //do something else
+                changeState(5); //oh no!
             }
         }, 2000);
-        
-
     };
 };
